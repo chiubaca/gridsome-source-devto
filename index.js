948ce9567aa78bed702cfb4d601e6561877db802
Alex Chiu <alexchiu11@gmail.com>
2021-01-03 16:53:57 +0000

    Get all dev articles via /articles/:id endpoint. This provides additional data for the article such as body_html and more. Then merge object with articels from articles/me/published endpoint to get best of both worlds

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require('axios')
-
+var _ = require('lodash');
 class DevtoSource {
   // Default options for the plugin
   static defaultOptions() {
@@ -64,17 +64,58 @@ class DevtoSource {
       return results.concat(resp.data);
     }
 
+    async function fetchArticleById(id) {
+      await sleep(100)
+      const articles = await axios.get(
+        `https://dev.to/api/articles/${id}`,
+      )
+      return articles
+    }
+    
+  /**
+   * 
+   * @param {array} articleIds - an array of dev.to article ids to lookup
+   */
+    async function fetchAllUsersArticlesById(articleIds){
+      // build up an array network requests
+      const idsToQuery =  articleIds.map(obj =>{
+        return fetchArticleById(obj)
+    })
+    
+    // repsonse of all networks requests 
+    const batchResp =  await Promise.all(idsToQuery)
+
+    // Cleanup of batch response to return an array or dev.to article objs
+    const allArticlesByID = batchResp.map(resp => resp.data)
+
+    return allArticlesByID
+
+    }
+
     api.loadSource(async ({ addCollection }) => {
 
       const allArticles = await fetchAllArticles()
 
-      console.log("Got Articels!", allArticles.length)
+      // contruct an array of article ids [123, 432, 123]
+      const allArticleIds = allArticles.map(obj => obj.id)
+
+      const allArticlesByID = await fetchAllUsersArticlesById(allArticleIds)
+
+      const merge = (obj1, obj2) => ({...obj1, ...obj2});
+
+      const mergedArticles =  _.zipWith(allArticlesByID, allArticles, merge)
+
+
+      // parse markdown
+      // for (const article of allArticles) {
+        
+      // }
 
       const collection = addCollection({
         typeName: options.typeName
       })
 
-      for (const article of allArticles) {
+      for (const article of mergedArticles) {
         collection.addNode({
           id: article.id,
           title: article.title,
@@ -83,6 +124,7 @@ class DevtoSource {
           slug: article.slug,
           url: article.url,
           body_markdown: article.body_markdown,
+          body_html: article.body_html,
           tags: article.tag_list,
           // Computed properties
         })
